bbcd1622454f9edad44ec4e2c83abd2c2107d302
momokahori124@gmail.com <momoka@mbp-2.local>
2020-10-30 18:13:25 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def hello
+        render html: "Hello World !!!"
+    end
+
+    def goodbye
+        render html: "Good Bye !!!"
+    end
 end
